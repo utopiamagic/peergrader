0e61df1bb7f5b6869912dc47bbb1590526de009c
David Ma <qingnian213@hotmail.com>
2016-12-20 11:58:40 -0800

    Added several functions for CourseBase

---
+++ peer_panel/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from peer_course.views import CourseBase
+from peer_auth.views import AuthBase
+import platform
+
+# Functions other than view functions should be placed elsewhere
+
+class PanelViews :
+	def homepage(request):
+		'Render the homepage'
+		render_dict = {
+			'courses': CourseBase.all(),
+			'logged_in': request.user.is_authenticated(),
+		}
+		if request.user.is_authenticated():
+			render_dict['lastname'] = request.user.last_name
+			render_dict['firstname'] = request.user.first_name
+		return render(request, 'index.html', render_dict)
